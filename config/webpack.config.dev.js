9b945a116631be6621e3e5ac71963af85d6ca693
Daan Vanoverloop <vanoverloopdaan@gmail.com>
2018-08-05 10:40:42 +0200

    Upgraded most packages, will do webpack next

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -240,6 +240,7 @@ module.exports = {
             loader: require.resolve('less-loader'),
             options: {
               // modifyVars: { "@primary-color": "#1DA57A" },
+              javascriptEnabled: true
             },
           },
         ],
